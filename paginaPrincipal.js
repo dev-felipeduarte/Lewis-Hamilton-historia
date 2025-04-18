c9ad05e3ba30d806d5e70b756e18059f0277e007
Felipe Duarte <profissionalfelipe.duarte@hotmail.com>
2025-04-17 21:49:35 -0300

    Site do Hamilton

---
+++ paginaPrincipal.js
@@ -0,0 +1,13 @@
+const myObserver = new IntersectionObserver( (animacao) => {
+  animacao.forEach( (entry) => {
+      if(entry.isIntersecting){
+          entry.target.classList.add('show')
+      } else {
+          entry.target.classList.remove('show')
+      }    
+  })
+})
+
+const imagem = document.querySelectorAll('.hamiltonFoto')
+
+imagem.forEach( (imagem) => myObserver.observe(imagem))
